c069cd0c2e6baa7d518b51cb1c768c8aeb9067eb
AnthonyP57 <138037189+AnthonyP57@users.noreply.github.com>
2024-01-06 19:49:57 +0100

    Update SHAP.py

--- SHAP.py
+++ SHAP.py
@@ -1,12 +1,14 @@
+"""SHAP"""
+
 import pandas as pd
 import xgboost
 import shap
 import matplotlib.pyplot as plt
 
-# Create DataFrame
+# Create the DataFrame
 data = {
     "WRPT": [0.7632, 0.4267, 1.6535, 2.1415, 0.3174, 0.8376, 1.4143],
-    "D_m":[.408, .482, .329, .309, .437, .348, .307],
+    "D_m": [.408, .482, .329, .309, .437, .348, .307],
     "a_m2/m3": [190, 305, 100, 80, 482, 206, 130],
     "E": [0.74554, 0.7, 0.8, 0.81, 0.928, 0.948, 0.96],
     "size_mm": [25, 15, 38, 50, 10, 25, 38],
